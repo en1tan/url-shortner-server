e4b29c7b9c4ae5c9612bf75a3114f94fd74e3a07
x1k <asajuenitan@gmail.com>
2021-01-31 21:47:27 +0100

    Fixed slug.js router

--- routes/slug.js
+++ routes/slug.js
@@ -5,14 +5,14 @@ const router = express.Router()
 const nanNumber = process.env.SLUG_NUM;
 
 // Generate slug
-router.post('/new', (req, res) => {
+router.post('/new', async (req, res) => {
     const { url } = req.body;
     try {
         const newUrl = new urlModel({
             slug: nanoid.nanoid(nanNumber),
             url: url.toLowerCase()
         });
-        newUrl.save();
+        await newUrl.save();
         return res.status(201).json({ success: true, data: newUrl.slug });
     } catch (err) {
         return res.status(500).json({ success: false, msg: `Error occured. Please try again ${err}` });
